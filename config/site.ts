a2abbd69819cb706f83ac4eec14dad509253302f
dstan97 <d.standerwick@hotmail.com>
2023-07-11 14:49:06 +0100

    updated site

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Minerva's Oddities",
+  description: "Handmade crafts and curated decorative products",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
